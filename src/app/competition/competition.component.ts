a7d28abe589b64ac1058582442604b92089a1f5f
Lee <solidpple@gmail.com>
2018-01-21 15:56:23 +0900

    add config

--- src/app/competition/competition.component.ts
+++ src/app/competition/competition.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CompetitionService } from './competition.service'
+import { DBConfig, COMP_DB_CONFIG } from './competition.config'
 
 @Component({
   selector: 'app-competition',
+  template: '{{ dbConfig | json }}',
+  /*
   template: `
     <button (click)="sayHi()">Say Hi</button>
     <p>{{ data }}</p>
   `,
+  */
   // templateUrl: './competition.component.html',
   styleUrls: ['./competition.component.scss'],
   providers: [
      { provide: CompetitionService, useClass: CompetitionService }
-    // ,{ provide: DBConfig, useValue: COMP_DB_CONFIG }
+     ,{ provide: DBConfig, useValue: COMP_DB_CONFIG }
   ]
 })
 export class CompetitionComponent implements OnInit {
 
   data: string;
 
-  constructor(private competitionService: CompetitionService) {
+  constructor(private competitionService: CompetitionService, public dbConfig: DBConfig) {
     // @Inject('myConfig') public myConfig: string
     // console.log(dbConfig);
   }
