cf2a48a9208f7872ea39d40344af3293c0bb4c04
Lee <solidpple@gmail.com>
2018-01-21 15:51:37 +0900

    add competition service

--- src/app/competition/competition.component.ts
+++ src/app/competition/competition.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CompetitionService } from './competition.service'
 
 @Component({
   selector: 'app-competition',
-  templateUrl: './competition.component.html',
-  styleUrls: ['./competition.component.scss']
+  template: `
+    <button (click)="sayHi()">Say Hi</button>
+    <p>{{ data }}</p>
+  `,
+  // templateUrl: './competition.component.html',
+  styleUrls: ['./competition.component.scss'],
+  providers: [
+     { provide: CompetitionService, useClass: CompetitionService }
+    // ,{ provide: DBConfig, useValue: COMP_DB_CONFIG }
+  ]
 })
 export class CompetitionComponent implements OnInit {
 
-  constructor() { }
+  data: string;
+
+  constructor(private competitionService: CompetitionService) {
+    // @Inject('myConfig') public myConfig: string
+    // console.log(dbConfig);
+  }
+
+  sayHi() {
+    this.data = this.competitionService.loadData();
+    this.data = 'gg';
+  }
 
   ngOnInit() {
   }
-
 }
---
+++ src/app/competition/competition.config.ts
@@ -0,0 +1,9 @@
+export class DBConfig {
+  url: string;
+  port: string;
+}
+
+export const COMP_DB_CONFIG: DBConfig = {
+  url: 'http://somewhere.io/api',
+  port: '5000'
+};
