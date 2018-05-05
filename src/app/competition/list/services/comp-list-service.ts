b58de2cb8f7216ccc62a64eb06976cda07372ebc
kLawrence <ultimate1352@gmail.com>
2018-05-05 20:26:42 +0900

    apply environment variables

--- src/app/competition/list/services/comp-list-service.ts
+++ src/app/competition/list/services/comp-list-service.ts
@@ -3,12 +3,17 @@ import { HttpClient} from '@angular/common/http';
 import { Competition } from '../domain/comp';
 import 'rxjs/add/operator/toPromise';
 
+import { environment } from '../../../../environments/environment';
+
 @Injectable()
 export class CompetitionListService{
     
-    private compsUrl = 'http://210.89.178.101:9000/competition' 
-    private local = 'assets/data/competition-list.json'
-    constructor(private http: HttpClient) {}
+    // private local = 'assets/data/competition-list.json'
+    private compsUrl = `${environment.api.zaggle}/competition`
+
+    constructor(private http: HttpClient) {
+
+    }
     
     getCompetitionList() {
         return this.http.get<any>(this.compsUrl)
